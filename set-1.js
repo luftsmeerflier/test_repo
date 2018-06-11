2fe11eb8a4697c3dc313e9b3e5f56508a5cee9cf
Olyskoop <snap@olymega.org>
2018-06-11 15:12:31 -0700

    added better error handling

--- set-1.js
+++ set-1.js
@@ -1,18 +1,19 @@
-const whoAmI = function(name = 'John' , age) {
-    const yob = yearOfBirth(age);
+const whoAmI = function(name, age) {
 
 	try {
-   		console.log(`My name is ${name} and I am ${age} years old`);
-    	console.log(`I was born in ${yob}`);
-		if (age < 0) throw new Error("Age can not be negative");
 		if (age === undefined || name === undefined) throw new Error("arguments not valid");
-		if (typeof age !== 'number' || typeof name !== 'string') throw new Error("Arguments invalid")
+		if (typeof age !== 'number' || typeof name !== 'string') throw new Error("Arguments invalid");
+		const yob = yearOfBirth(age);
+   		console.log(`My name is ${name} and I am ${age} years old`);
+  		console.log(`I was born in ${yob}`);
+			
 	} catch(e) {
 		console.error(e.message);
 	}
 }
 const yearOfBirth = function(age){
+	if (age < 0) throw new Error("Age can not be negative");
 	return 2018 - age;
 }
 
-whoAmI("olyskoop", "twenty");
+whoAmI("olyskoop", -20);
