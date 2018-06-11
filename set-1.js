253d4518a1eea9f83162fb39e4ae22dd93c1e776
Akim Desire <adesi006.ng@gmail.com>
2018-06-11 16:27:08 -0400

    added mad error messages

--- set-1.js
+++ set-1.js
@@ -1,19 +1,18 @@
-const whoAmI = function(name = 'John' , age = 20) {
+const whoAmI = function(name = 'John' , age) {
     const yob = yearOfBirth(age);
 
 	try {
    		console.log(`My name is ${name} and I am ${age} years old`);
     	console.log(`I was born in ${yob}`);
-    	if (age < 0) throw new Error("Age can not be negative");
-
+		if (age < 0) throw new Error("Age can not be negative");
+		if (age === undefined || name === undefined) throw new Error("arguments not valid");
+		if (typeof age !== 'number' || typeof name !== 'string') throw new Error("Arguments invalid")
 	} catch(e) {
 		console.error(e.message);
 	}
 }
-
-
 const yearOfBirth = function(age){
 	return 2018 - age;
 }
 
-whoAmI("olyskoop", -5);
+whoAmI("olyskoop", "twenty");
