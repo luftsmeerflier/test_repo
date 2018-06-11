fa387d4e3a54e9aa86681cd86ce4d8acb8239785
Olyskoop <snap@olymega.org>
2018-06-11 13:04:32 -0700

    added throw exceptions :

--- set-1.js
+++ set-1.js
@@ -1,7 +1,14 @@
 const whoAmI = function(name = 'John' , age = 20) {
     const yob = yearOfBirth(age);
-    console.log(`My name is ${name} and I am ${age} years old`);
-    console.log(`I was born in ${yob}`);
+
+	try {
+   		console.log(`My name is ${name} and I am ${age} years old`);
+    	console.log(`I was born in ${yob}`);
+    	if (age < 0) throw new Error("Age can not be negative");
+
+	} catch(e) {
+		console.error(e.message);
+	}
 }
 
 
@@ -9,4 +16,4 @@ const yearOfBirth = function(age){
 	return 2018 - age;
 }
 
-whoAmI();
+whoAmI("olyskoop", -5);
